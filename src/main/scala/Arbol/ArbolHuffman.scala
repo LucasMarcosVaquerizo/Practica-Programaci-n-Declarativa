cb1df96066134959cadc28fae0a6c0bef5fa5cc2
LucasMarcosVaquerizo <157593171+LucasMarcosVaquerizo@users.noreply.github.com>
2024-10-30 15:30:25 +0100

    función decodificar dentro de ArbolHuff terminada

--- src/main/scala/Arbol/ArbolHuffman.scala
+++ src/main/scala/Arbol/ArbolHuffman.scala
@@ -1,5 +1,7 @@
 package Arbol
 
+import scala.annotation.tailrec
+
 type Bit = 0 | 1
 
 
@@ -14,6 +16,24 @@ trait ArbolHuffman {
       case ramaHuff(nodoIzq: ArbolHuffman, nodoDch: ArbolHuffman) => caracteresAux(nodoIzq, lista) ::: caracteresAux(nodoDch, lista)
     caracteresAux(this, Nil)
 
+  def cadenaAListaChars(cadena: String): List[Char] =
+    cadena.toList
+
+  private def listaCharsACadena(listaCar: List[Char]): String =
+    listaCar.mkString
+
+  def decodificar (bits: List[Bit]): String =
+    @tailrec
+    def decodificarAux(arbol: ArbolHuffman, bits: List[Bit], listaChar: List[Char]): List[Char] = this match
+      case hojaHuff(caracter: Char, peso: Int) => caracter::listaChar
+      case ramaHuff(nodoIzq: ArbolHuffman, nodoDch: ArbolHuffman) => bits.head match
+        case 0 => decodificarAux(nodoIzq, bits.tail, listaChar)
+        case 1 => decodificarAux(nodoDch, bits.tail, listaChar)
+    
+    listaCharsACadena(decodificarAux(this, bits, Nil))
+    
+      
+      
 }
 case class hojaHuff(caracter: Char, pes: Int) extends ArbolHuffman
 
