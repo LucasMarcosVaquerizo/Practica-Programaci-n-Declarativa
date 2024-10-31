6a13357d91a9b21dbd2dd12bfb41619ff7044e69
LucasMarcosVaquerizo <157593171+LucasMarcosVaquerizo@users.noreply.github.com>
2024-10-31 16:30:25 +0100

    función de codificación y ejemplos implementados

--- src/main/scala/Arbol/ArbolHuffman.scala
+++ src/main/scala/Arbol/ArbolHuffman.scala
@@ -1,6 +1,7 @@
 package Arbol
 
 import scala.annotation.tailrec
+import scala.jdk.CollectionConverters.*
 
 type Bit = 0 | 1
 
@@ -16,7 +17,7 @@ trait ArbolHuffman {
       case ramaHuff(nodoIzq: ArbolHuffman, nodoDch: ArbolHuffman) => caracteresAux(nodoIzq, lista) ::: caracteresAux(nodoDch, lista)
     caracteresAux(this, Nil)
 
-  def cadenaAListaChars(cadena: String): List[Char] =
+  private def cadenaAListaChars(cadena: String): List[Char] =
     cadena.toList
 
   private def listaCharsACadena(listaCar: List[Char]): String =
@@ -32,7 +33,26 @@ trait ArbolHuffman {
 
     listaCharsACadena(decodificarAux(this, bits, Nil))
 
+  private def arbolContiene(caracter:Char): Boolean =
+    def arbolContieneAux(arbol: ArbolHuffman, caracter: Char, cond: Boolean): Boolean = arbol match
+      case hojaHuff(c, _) => c == caracter
+      case ramaHuff(nodoIzq, nodoDch) => nodoIzq.arbolContiene(caracter) || nodoDch.arbolContiene(caracter)
 
+
+
+    arbolContieneAux(this, caracter, false)
+
+  def codificar(cadena: String): List[Bit] =
+    @tailrec
+    def codificarAux(arbol: ArbolHuffman, listaChar: List[Char], listaBits: List[Bit]): List[Bit] = arbol match
+      case hojaHuff(c, _)
+        if c == listaChar.head && listaChar.tail != Nil => codificarAux(this, listaChar.tail, listaBits)
+      case hojaHuff(c, _) if c == listaChar.head => listaBits
+      case ramaHuff(nodoIzq, _) if nodoIzq.arbolContiene(listaChar.head) => codificarAux(nodoIzq, listaChar, 0::listaBits)
+      case ramaHuff(_, nodoDch) if nodoDch.arbolContiene(listaChar.head) => codificarAux(nodoDch, listaChar, 1::listaBits)
+
+
+    codificarAux(this, cadenaAListaChars(cadena), Nil).reverse
 
 }
 case class hojaHuff(caracter: Char, pes: Int) extends ArbolHuffman
@@ -42,8 +62,10 @@ case class ramaHuff(nodoIzq: ArbolHuffman, nodoDch: ArbolHuffman) extends ArbolH
 
 @main
 def main(): Unit= {
-  val arbol: ArbolHuffman = ramaHuff(hojaHuff('s', 6), hojaHuff('a', 4))
+  val arbol: ArbolHuffman = ramaHuff(hojaHuff('S', 4), ramaHuff(hojaHuff('O', 3), ramaHuff(hojaHuff('E', 2), hojaHuff(' ', 2))))
   println(arbol.peso)
+  println(arbol.codificar("ESO ES OSOS"))
+
 
 }
 
