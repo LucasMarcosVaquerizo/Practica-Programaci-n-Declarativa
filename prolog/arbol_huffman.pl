839c060355f8d2e97952dfb6e2792565aa8da960
jaizu23 <157589155+jaizu23@users.noreply.github.com>
2024-12-20 13:52:19 +0100

    Update arbol_huffman.pl
    Added arbol prueba, es_arbol, peso y lista_caracteres

--- prolog/arbol_huffman.pl
+++ prolog/arbol_huffman.pl
@@ -1,1 +1,18 @@
-df
+mi_arbol(rama(hoja('S',4),rama(hoja('O',3),rama(hoja('E',2),hoja(' ',2))))).
+
+es_arbol(hoja(_,_)).
+es_arbol(rama(I,D)) :-
+    es_arbol(I),
+    es_arbol(D).
+
+peso(hoja(_,P),P).
+peso(rama(I,D),P) :-
+    peso(I,PI),
+    peso(D,PD),
+    P is PI+PD.
+
+lista_caracteres(hoja(L,_),[L]).
+lista_caracteres(rama(I,D),L) :-
+    lista_caracteres(I,LI),
+    lista_caracteres(D,LD),
+    append(LI,LD,L).
