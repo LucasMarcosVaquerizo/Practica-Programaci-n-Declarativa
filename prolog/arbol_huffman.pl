5d6a9b65b76470175ea2176d3830280ce0610978
LucasMarcosVaquerizo <157593171+LucasMarcosVaquerizo@users.noreply.github.com>
2024-12-22 12:07:19 +0100

    cambios

--- prolog/arbol_huffman.pl
+++ prolog/arbol_huffman.pl
@@ -16,3 +16,37 @@ lista_caracteres(rama(I,D),L) :-
     lista_caracteres(I,LI),
     lista_caracteres(D,LD),
     append(LI,LD,L).
+
+
+char_codigohuff(Codigo, Caracter):- mi_arbol(Arbol), 
+char_codigohuff_arbol(Codigo, Arbol, Caracter).
+
+
+char_codigohuff_arbol([],hoja(P,_), P).
+char_codigohuff_arbol([0|Xs], rama(X1,_),P):- char_codigohuff_arbol(Xs,X1,P).
+char_codigohuff_arbol([1|Xs], rama(_,X1),P):- char_codigohuff_arbol(Xs,X1,P).
+
+
+texto_a_bits(Texto, StringBits) :-
+atom_chars(Texto, ListaChars),
+texto_a_bits_lista(ListaChars, Bits),
+maplist(number_chars, Bits, ListaBitsChars),
+flatten(ListaBitsChars, ListaBitsFlatten),
+atomic_list_concat(ListaBitsFlatten, '', AtomicBits),
+atom_string(AtomicBits,StringBits).
+
+
+
+texto_a_bits_lista([],[]).
+texto_a_bits_lista([X|Xs], Zs):- char_codigohuff(Codigo, X), texto_a_bits_lista(Xs,Ys), append(Codigo, Ys, Zs).
+
+
+bits_a_texto(BitsString, Texto) :-
+ string_chars(BitsString, ListaBitsChars),
+ maplist(atom_number, ListaBitsChars, Bits),
+ bits_a_texto_lista(Bits, ListaChars),
+ string_chars(Texto, ListaChars).
+
+bits_a_texto_lista([], []).  
+bits_a_texto_lista(Bits, [Char|Chars]) :- mi_arbol(Arbol), char_codigohuff_arbol(Codigo, Arbol, Char), append(Codigo, RestoBits, Bits), bits_a_texto_lista(RestoBits, Chars).
+
